f764fa44184896b585336c2250d1aabde064abbc
amstevenson <addstevenson@hotmail.com>
2019-04-28 15:13:22 +0100

    added image gallery for selected game, css is a bit off

--- game_searcher_frontend/src/components/SelectedGame/Game/Game.js
+++ game_searcher_frontend/src/components/SelectedGame/Game/Game.js
@@ -24,102 +24,47 @@ const selectedGame = (props) => {
     /* Sizes of image are t_cover_big, t_original_ t_thumb */
     const screenshots = (typeof props.screenshots !== 'undefined') ? props.screenshots : null;
 
-    const imageGallery = props.screenshots.map((screenshot, index) => {
+    const imageGalleryPictures = props.screenshots.map((screenshot, index) => {
 
-        // index is indexed, starts at 0
-        // length is not indexed, starts at 1
+        const imageIndex = (index+1);
 
         return <Fragment key={screenshot.id}>
-            <input type="radio" name="radio-btn" id={screenshot.id} checked />
-            <li class="slide-container">
-                <div class="slide">
+
+            {imageIndex === 1 ? <input type="radio" name="radio-btn" id={"img-" + imageIndex } defaultChecked /> : 
+                <input type="radio" name="radio-btn" id={"img-" + imageIndex } /> }
+
+            <li className="slide-container">
+                <div className="slide">
                         <img src={screenshot.url.replace("t_thumb", "t_original")} />
                 </div>
-                <div class="slideNav">
-                    <label for="img-6" class="prev">&#x2039;</label>
-                    <label for="img-2" class="next">&#x203a;</label>
+                <div className="slideNav">
+                    <label for={imageIndex === 1 ? 
+                        "img-" + props.screenshots.length : "img-" + (imageIndex - 1)} className="prev">&#x2039;</label>
+
+                    <label for={imageIndex === props.screenshots.length ?
+                        "img-1" : "img-" + (imageIndex + 1)} className="next">&#x203a;</label>
                 </div>
             </li>
         </Fragment>
     });
 
+    const imageGalleryDots = props.screenshots.map((screenshot, index) => {
+        
+        const imageIndex = (index+1)
 
-    // <ul class="slides">
-    //     <input type="radio" name="radio-btn" id="img-1" checked />
-    //     <li class="slide-container">
-    //         <div class="slide">
-    //             <img src="http://farm9.staticflickr.com/8072/8346734966_f9cd7d0941_z.jpg" />
-    //         </div>
-    //         <div class="nav">
-    //             <label for="img-6" class="prev">&#x2039;</label>
-    //             <label for="img-2" class="next">&#x203a;</label>
-    //         </div>
-    //     </li>
+        return <label for={"img-" + imageIndex} className="slideNav-dot" id={"img-dot-" + imageIndex}></label>
+           
+    });
 
-    //     <input type="radio" name="radio-btn" id="img-2" />
-    //     <li class="slide-container">
-    //         <div class="slide">
-    //         <img src="http://farm9.staticflickr.com/8504/8365873811_d32571df3d_z.jpg" />
-    //         </div>
-    //         <div class="nav">
-    //             <label for="img-1" class="prev">&#x2039;</label>
-    //             <label for="img-3" class="next">&#x203a;</label>
-    //         </div>
-    //     </li>
+    const imageGallery = screenshots ? <ul className="slides">
+    
+        {imageGalleryPictures}
 
-    //     <input type="radio" name="radio-btn" id="img-3" />
-    //     <li class="slide-container">
-    //         <div class="slide">
-    //         <img src="http://farm9.staticflickr.com/8068/8250438572_d1a5917072_z.jpg" />
-    //         </div>
-    //         <div class="nav">
-    //             <label for="img-2" class="prev">&#x2039;</label>
-    //             <label for="img-4" class="next">&#x203a;</label>
-    //         </div>
-    //     </li>
+        <li className="slideNav-dots">
+            {imageGalleryDots}
+        </li>
 
-    //     <input type="radio" name="radio-btn" id="img-4" />
-    //     <li class="slide-container">
-    //         <div class="slide">
-    //         <img src="http://farm9.staticflickr.com/8061/8237246833_54d8fa37f0_z.jpg" />
-    //         </div>
-    //         <div class="nav">
-    //             <label for="img-3" class="prev">&#x2039;</label>
-    //             <label for="img-5" class="next">&#x203a;</label>
-    //         </div>
-    //     </li>
-
-    //     <input type="radio" name="radio-btn" id="img-5" />
-    //     <li class="slide-container">
-    //         <div class="slide">
-    //         <img src="http://farm9.staticflickr.com/8055/8098750623_66292a35c0_z.jpg" />
-    //         </div>
-    //         <div class="nav">
-    //             <label for="img-4" class="prev">&#x2039;</label>
-    //             <label for="img-6" class="next">&#x203a;</label>
-    //         </div>
-    //     </li>
-
-    //     <input type="radio" name="radio-btn" id="img-6" />
-    //     <li class="slide-container">
-    //         <div class="slide">
-    //         <img src="http://farm9.staticflickr.com/8195/8098750703_797e102da2_z.jpg" />
-    //         </div>
-    //         <div class="nav">
-    //             <label for="img-5" class="prev">&#x2039;</label>
-    //             <label for="img-1" class="next">&#x203a;</label>
-    //         </div>
-    //     </li>
-
-    //     <li class="nav-dots">
-    //     <label for="img-1" class="nav-dot" id="img-dot-1"></label>
-    //     <label for="img-2" class="nav-dot" id="img-dot-2"></label>
-    //     <label for="img-3" class="nav-dot" id="img-dot-3"></label>
-    //     <label for="img-4" class="nav-dot" id="img-dot-4"></label>
-    //     <label for="img-5" class="nav-dot" id="img-dot-5"></label>
-    //     <label for="img-6" class="nav-dot" id="img-dot-6"></label>
-    //     </li>
-    // </ul>
+    </ul> : null
 
     return (
         <Fragment>
